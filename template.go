a941b11570999c364155380cc40605faa62af02e
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-01-23 14:09:28 +0100

    add func map to template for time manipulation

--- template.go
+++ template.go
@@ -4,14 +4,35 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
+
+	"github.com/dustin/go-humanize"
 )
+
+func add(a, b int) int {
+	return a + b
+}
+
+func float64ToTime(v float64) time.Time {
+	return time.Unix(int64(v), 0)
+}
+
+func float64ToHuman(v float64) string {
+	return humanize.Time(float64ToTime(v))
+}
+
+var funcMap = template.FuncMap{
+	"add":            add,
+	"float64ToTime":  float64ToTime,
+	"float64ToHuman": float64ToHuman,
+}
 
 type AppTemplate struct {
 	t *template.Template
 }
 
 func NewAppTemplate(files ...string) *AppTemplate {
-	base := template.Must(template.New("base").ParseFiles("base.html"))
+	base := template.Must(template.New("base").Funcs(funcMap).ParseFiles("base.html"))
 	template.Must(base.ParseFiles(files...))
 	return &AppTemplate{base}
 }
