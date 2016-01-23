fdbb9911060a0c81457756fe2276bd511833ee6b
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-01-23 14:27:10 +0100

    init xcode project

---
+++ todo/todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo
+//
+//  Created by Mirco Zeiss on 1/23/16.
+//  Copyright © 2016 zemirco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
