104ed8f9ba2a2daf017d5d503959a11b40b91b4f
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-01-23 19:44:21 +0100

    add textfield to add todo viewcontroller

--- todo/todo/AddTodoViewController.swift
+++ todo/todo/AddTodoViewController.swift
@@ -2,6 +2,8 @@
 import UIKit
 
 class AddTodoViewController: UIViewController {
+    
+    private var textfield: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,6 +11,21 @@ class AddTodoViewController: UIViewController {
         self.title = "Add todo"
         self.view.backgroundColor = UIColor.whiteColor()
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: "cancel")
+        
+        // add textfield
+        textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.placeholder = "take the dog for a walk"
+        self.view.addSubview(textfield)
+        
+        let views: [String: AnyObject] = [
+            "topLayoutGuide": self.topLayoutGuide,
+            "textfield": textfield
+        ]
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[textfield]-|", options: [], metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[topLayoutGuide]-50-[textfield]", options: [], metrics: nil, views: views))
+
 
     }
 
